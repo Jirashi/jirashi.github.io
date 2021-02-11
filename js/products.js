bf3e7131ff22dbd1d496b0153520c540d715cd18
Grim <Grqm@users.noreply.github.com>
2021-02-11 15:42:39 -0600

    beta v2.1

--- js/products.js
+++ js/products.js
@@ -2,7 +2,7 @@ var products = [
     {
         name: "Ez QR Code",
         description: "Ez QR Code is an easy to use free QR Code generator.",
-        href: "/EzQRCODE/index.html",
+        href: "/EzQRCODE",
         tags: ["qrcode", "generator", "link", "qr"]
     }
 ]
