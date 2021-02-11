f6c060d1dc0e643a69f12bc7e39076f8c79d422d
Grim <Grqm@users.noreply.github.com>
2021-02-11 16:37:05 -0600

    beta v1.3

--- js/products.js
+++ js/products.js
@@ -2,7 +2,13 @@ var products = [
     {
         name: "Ez QR Code",
         description: "Ez QR Code is an easy to use free QR Code generator.",
-        href: "/EzQRCODE",
+        href: "EzQRCODE",
         tags: ["qrcode", "generator", "link", "qr"]
+    },
+    {
+        name: "Notepad Pro",
+        description: "Notepad Pro is a rich text editor that allows you to edit and save notes online.",
+        href: "Notepad-Pro",
+        tags: ["notes", "notepad", "text", "editor"]
     }
 ]
